c9105eb1ad1b87e8bc135e3d500005ddfdf66611
Saranjith <saranjith@metrictreelabs.com>
2024-08-12 22:59:28 +0530

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hey server!!");
+  })
+  .listen(3000, () => {
+    console.log(`[server] is running at http://localhost:${3000}`);
+  });
