f83c0135f9ea874357b6c6b99ba91df9aa2afe4e
Saranjith <saranjith@metrictreelabs.com>
2024-08-22 17:46:44 +0530

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hey server!!"); 
+    res.end("Hey deploy test!!");
   })
   .listen(3000, () => {
     console.log(`[server] is running at http://localhost:${3000}`);
